a28cb5da889b7802b7c288890a310c499e719981
gustapoll <gustavo.polleti@nubank.com.br>
2020-03-09 21:33:10 -0300

    fix/Make methods static

--- elric/features/dictionarizer.py
+++ elric/features/dictionarizer.py
@@ -1,14 +1,13 @@
-def create_dict(df, key):
-    return df.set_index(key).T.to_dict()
-
-
 class KnowledgeBaseDictionarizer(object):
 
+    def __create_dict(df, key):
+        return df.set_index(key).T.to_dict()
+
     def entity2id(df):
-        return create_dict(df, 'entity')
+        return KnowledgeBaseDictionarizer.__create_dict(df, 'entity')
 
     def id2entity(df):
-        return create_dict(df, 'id')
+        return KnowledgeBaseDictionarizer.__create_dict(df, 'id')
 
     def relation2id(df):
-        return create_dict(df, 'relation')
+        return KnowledgeBaseDictionarizer.__create_dict(df, 'relation')
--- elric/features/loader.py
+++ elric/features/loader.py
@@ -3,17 +3,16 @@ import pandas as pd
 from os import path
 
 
-def read_file(file, columns):
-    return pd.read_csv(file, sep='\t', header=None, skiprows=[0], names=columns)
-
-
 class KnowledgeBaseLoader(object):
 
+    def __read_file(file, columns):
+        return pd.read_csv(file, sep='\t', header=None, skiprows=[0], names=columns)
+
     def load_entities(directory):
-        return read_file(path.join(directory, 'entity2id.txt'), ['entity', 'id'])
+        return KnowledgeBaseLoader.__read_file(path.join(directory, 'entity2id.txt'), ['entity', 'id'])
 
     def load_relations(directory):
-        return read_file(path.join(directory, 'relation2id.txt'), ['relation', 'id'])
+        return KnowledgeBaseLoader.__read_file(path.join(directory, 'relation2id.txt'), ['relation', 'id'])
 
     def load_facts(directory):
-        return read_file(path.join(directory, 'train2id.txt'), ['head', 'tail', 'relation'])
+        return KnowledgeBaseLoader.__read_file(path.join(directory, 'train2id.txt'), ['head', 'tail', 'relation'])
